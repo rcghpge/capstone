5f0bc6c6d0afc3a71c2115a70923dda9ba165a8b
Robert Cocker <rcocker87@gmail.com>
2026-02-05 20:35:31 -0600

    devcontainer: post build scaffolding for codespaces environments2

---
+++ .devcontainer/post-build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+/opt/conda/bin/python -m pip install --upgrade ipykernel
+/opt/conda/bin/python -m ipykernel install --sys-prefix --name capstone-python3.13 --display-name "Capstone Python 3.13"
+/opt/conda/bin/python -m pip install -e .[dev]
+
+jupyter lab --generate-config --allow-root
+echo 'c.ServerApp.ip = "0.0.0.0"' >> ~/.jupyter/jupyter_lab_config.py
+echo 'c.ServerApp.token = ""' >> ~/.jupyter/jupyter_lab_config.py
+
+code .
