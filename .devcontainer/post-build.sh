e9ed06a1bd1db0e56c043d3fb3c1775cd5c5378b
Robert Cocker <rcocker87@gmail.com>
2026-02-14 16:15:07 -0600

    devcontainers: vscode/codespaces env builds. wip. check envs. wip

--- .devcontainer/post-build.sh
+++ .devcontainer/post-build.sh
@@ -4,14 +4,16 @@ set -e
 echo 'export PATH="/home/jovyan/.local/bin:/opt/conda/bin:/usr/local/bin:/usr/bin:/bin"' > ~/.bashrc
 source ~/.bashrc
 
-/opt/conda/bin/python -m pip install --upgrade ipykernel
+/opt/conda/bin/python -m pip install --upgrade --user ipykernel
 /opt/conda/bin/python -m ipykernel install --sys-prefix --name capstone-python3.13 --display-name "Capstone Python 3.13"
-/opt/conda/bin/python -m pip install -e .[dev]
+/opt/conda/bin/python -m pip install --user -e .[dev]
 rm -rf *.egg-info
 
-jupyter lab --generate-config --allow-root
+jupyter lab --generate-config --allow-root || true
 echo 'c.ServerApp.ip = "0.0.0.0"' >> ~/.jupyter/jupyter_lab_config.py
 echo 'c.ServerApp.token = ""' >> ~/.jupyter/jupyter_lab_config.py
 
 conda init bash --user
 echo 'conda activate base' >> ~/.bashrc
+
+echo "✅ Setup done!"
